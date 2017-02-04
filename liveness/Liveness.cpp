4dc6333b8e57fcec4b79a52cac4abe132e4e9c64
Bikash Gogoi <bikashgogoi001@gmail.com>
2017-02-04 16:08:08 +0530

    working for simple cases

--- liveness/Liveness.cpp
+++ liveness/Liveness.cpp
@@ -46,6 +46,7 @@ bool Liveness::runOnModule(Module &M) {
 void Liveness::computeLiveness(Function &F) {
 	std::map<std::string,BlockData> bblocks;
 	std::deque<BasicBlock*> worklist;
+	BasicBlock *print_block = NULL;
 
 	for (Function::iterator BB = F.begin(), E = F.end(); BB != E; ++BB) {
 		worklist.push_back(dyn_cast<BasicBlock>(BB));
@@ -71,11 +72,24 @@ void Liveness::computeLiveness(Function &F) {
 						Value *val = dyn_cast<Value>(I->getOperand(0));
 						if (val->hasName()) {
 							std::string str = val->getName();
-							if (def.find(str) == def.end())
+							if (def.find(str) == def.end()) {
 								use.insert(str);
+							}
 						}
 						// LoadInst *LI = dyn_cast<LoadInst>(I);
 						// LI->print(outs()); outs() << "\n";
+						break;
+					}
+				case Instruction::Call:
+					{
+						CallInst *CI = dyn_cast<CallInst>(I);
+						Function *f = CI->getCalledFunction();
+						std::string fname = f->getName();
+						// outs() << fname << " fnc\n";
+						if (f != NULL && fname.compare("printf") == 0) {
+							// outs() << "\ninside printf\n";
+							print_block = dyn_cast<BasicBlock>(BB);
+						}
 						break;
 					}
 				default:
@@ -91,10 +105,10 @@ void Liveness::computeLiveness(Function &F) {
 	do {
 		BasicBlock &basic_block = *(worklist.front());
 		worklist.pop_front();
-		BlockData &block_data = bblocks[basic_block.getName()];
+		BlockData &block_data = bblocks.at(basic_block.getName());
 		in_size = block_data.in.size();
 		for (succ_iterator SI = succ_begin(&basic_block), E = succ_end(&basic_block); SI != E; ++SI) {
-			setptr = &(bblocks[SI->getName()].in);
+			setptr = &(bblocks.at(SI->getName()).in);
 			block_data.out.insert(setptr->begin(),setptr->end());
 		}
 		block_data.in.insert(block_data.use.begin(), block_data.use.end());
@@ -108,15 +122,96 @@ void Liveness::computeLiveness(Function &F) {
 		}
 	} while (!worklist.empty());
 
-	// for (Function::iterator BB = F.begin(), E = F.end(); BB != E; ++BB) {
-		
-	// }
-	
-	// std::set<std::string>::iterator b, e;
-	// for (b=vars.begin(), e=vars.end(); b != e; ++b) {
-	// 	outs() << *b << " ";
-	// }
-	// outs()<< "\n";
+	if (print_block == NULL) {
+		outs() << "\nnull!!! not able to find print basic block\n";
+		return;
+	}
+
+	BlockData &print_block_data = bblocks.at(print_block->getName());
+	std::set<std::string> live_at_print(print_block_data.out.begin(), print_block_data.out.end());
+	std::set<std::string> def, use;
+	std::map<Value*, std::set<std::string> > track_data;
+	bool after_call = false;
+
+	for (BasicBlock::iterator I = print_block->begin(), E = print_block->end(); I != E; ++I) {
+		switch (I->getOpcode()) {
+				case Instruction::Store:
+					{
+						Value *val = dyn_cast<Value>(I->getOperand(1));
+						if (val->hasName()) {
+							def.insert(val->getName());
+							std::map<Value*,std::set<std::string> >::iterator it = track_data.find(dyn_cast<Value>(I->getOperand(0)));
+							if (it != track_data.end()) {
+								track_data.erase(it);
+							}
+						}
+						break;
+					}
+				case Instruction::Load:
+					{
+						Value *val = NULL;
+						if (after_call) {
+							val = dyn_cast<Value>(I->getOperand(0));
+							if (val->hasName()) {
+								std::string str = val->getName();
+								if (def.find(str) == def.end()) {
+									live_at_print.insert(str);
+								}
+							}
+						} else {
+							val = dyn_cast<Value>(I->getOperand(0));
+							if (val->hasName()) {
+								std::string str = val->getName();
+								std::set<std::string> temp;
+								temp.insert(str);
+								track_data.insert(std::pair<Value*,std::set<std::string> > (dyn_cast<Value>(I), temp));
+							}
+						}
+						break;
+					}
+				case Instruction::Call:
+					{
+						if (!after_call) {
+							CallInst *CI = dyn_cast<CallInst>(I);
+							Function *f = CI->getCalledFunction();
+							std::string fname = f->getName();
+							if (f != NULL && fname.compare("printf") == 0) {
+								after_call = true;
+								std::map<Value*, std::set<std::string> >::iterator itr;
+								for (Instruction::op_iterator it=CI->arg_begin(), end=CI->arg_end(); it != end; ++it) {
+									itr = track_data.find(dyn_cast<Value>(it));
+									if (itr != track_data.end()) {
+										live_at_print.insert(itr->second.begin(), itr->second.end());
+									}
+								}
+								track_data.clear();
+							}
+						}
+						break;
+					}
+				default:
+					{
+						if (!after_call) {
+							std::set<std::string> temp;
+							std::map<Value*, std::set<std::string> >::iterator itr;
+							for (Instruction::value_op_iterator val=I->value_op_begin(), end=I->value_op_end(); val != end; ++val) {
+								itr = track_data.find(dyn_cast<Value>(*val));
+								if (itr != track_data.end()) {
+									temp.insert(itr->second.begin(), itr->second.end());
+									track_data.erase(itr);
+								}
+							}
+							track_data.insert(std::pair<Value*,std::set<std::string> >(dyn_cast<Value>(I), temp));
+						}
+						break;
+					}
+			}
+	}
+
+	for (std::set<std::string>::iterator b=live_at_print.begin(), e=live_at_print.end(); b != e; ++b) {
+		outs() << *b << " ";
+	}
+	outs()<< "\n";
 }
 
 
